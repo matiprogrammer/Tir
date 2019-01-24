9896bc37b5098ec49d14809a2d2d4000d949672e
Kiel97 <kkielek97@gmail.com>
2019-01-24 13:02:07 +0100

    Dodano commanda pod przycisk dodaj/edytuj oraz prostą walidację długości tekstu w kontrolkach dla firm

---
+++ TIR/Commands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TIR
+{
+    public class Commands
+    {
+        public static RoutedUICommand AddEditCompanyCommmand { get; private set; }
+        static Commands()
+        {
+            AddEditCompanyCommmand = new RoutedUICommand("Add or Edit Company", "AddEditCompany", typeof(Commands));
+        }
+    }
+}
