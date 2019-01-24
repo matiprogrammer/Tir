f2f22ff4495aa240ca61209e4027d27b9c5e1756
Kiel97 <kkielek97@gmail.com>
2019-01-24 18:45:39 +0100

    Dodano walidację danych w pracowniku (tak, poprzednio zapomniałem tego zrobić)

--- TIR/NewEditEmploye.xaml.cs
+++ TIR/NewEditEmploye.xaml.cs
@@ -71,6 +71,83 @@ namespace TIR
             string adres_zatrudnienia = address2Box.Text;
 
             #region Walidacja danych pracownika
+            if (nr_pesel.Length != 11)
+            {
+                MessageBox.Show("Numer PESEL pracownika musi składać się z dokładnie 11 cyfr!", "Nieprawidłowy numer PESEL", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (nazwisko.Length < 2)
+            {
+                MessageBox.Show("Nazwisko pracownika musi zawierać przynajmniej 2 znaki!", "Zbyt krótkie nazwisko pracownika", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (nazwisko.Length > 50)
+            {
+                MessageBox.Show("Nazwisko pracownika nie może być dłuższe niż 50 znaków!", "Za długie nazwisko pracownika", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (imie.Length < 2)
+            {
+                MessageBox.Show("Imię pracownika musi zawierać przynajmniej 2 znaki!", "Zbyt krótkie imię pracownika", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (imie.Length > 30)
+            {
+                MessageBox.Show("Imię pracownika nie może być dłuższe niż 30 znaków!", "Za długie imię pracownika", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (pensja >= 10000000000)
+            {
+                MessageBox.Show("Podana pensja pracownika jest za duża!", "Zbyt wysoka pensja pracownika", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (pensja < 0)
+            {
+                MessageBox.Show("Pensja pracownika nie może być wartością ujemną!", "Ujemna pensja pracownika", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (stanowisko.Length < 3)
+            {
+                MessageBox.Show("Nazwa stanowiska pracownika musi mieć conajmniej 3 znaki!", "Zbyt krótka nazwa stanowiska" , MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (stanowisko.Length > 50)
+            {
+                MessageBox.Show("Podana nazwa stanowiska przekracza 50 znaków!", "Za długa nazwa stanowiska", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            if (adres_zamieszkania.Length < 8)
+            {
+                MessageBox.Show("Adres zamieszkania pracownika musi mieć conajmniej 8 znaków!", "Zbyt krótki adres zamieszkania", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (adres_zamieszkania.Length > 100)
+            {
+                MessageBox.Show("Podany adres zamieszkania nie może być dłuższy niż 100 znaków!", "Za długi adres zamieszkania", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (adres_zatrudnienia.Length < 8)
+            {
+                MessageBox.Show("Adres zatrudnienia pracownika musi mieć conajmniej 8 znaków!", "Zbyt krótki adres zatrudnienia", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (adres_zatrudnienia.Length > 100)
+            {
+                MessageBox.Show("Podany adres zatrudnienia nie może być dłuższy niż 100 znaków!", "Za długi adres zatrudnienia", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
             #endregion
 
             Queries query = new Queries();
